b231fc7a596d2dbbf2f5cd7489a94b47b2dd67db
LeandroDSAndrade <andradedsleandro@gmail.com>
2023-12-10 00:58:01 -0300

    Inclusão de conversor de temperatura simples °C => °F

---
+++ Conversor de Temperatura/Conversos de Temperatura.py
@@ -0,0 +1,10 @@
+# Conversor de temperatura
+
+# ºC para °F
+
+temperatura_Celsius = float(input("Digite a temperatura em °C: \n"))
+
+temperatura_Fahrenheit = ((9 * temperatura_Celsius) / 5) + 32
+
+print("A temperatura em Celsius é: %.2f\n" % temperatura_Celsius)
+print("A temperatura em Fahrenheit é: %.2f\n" % temperatura_Fahrenheit)
---
+++ Conversor de Temperatura/README.txt
@@ -0,0 +1,1 @@
+Conversor de temperatura °C para °F (Celsius para Fahrenheit)
